a3bc96da76cfcd1e116c1201034d66c8ec99d444
Evgeny Zaryanov <eugenezaryanov@yandex.ru>
2024-02-22 14:19:19 +0300

    new updates

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.querySelectorAll('.trailers__button').forEach(button => {
+  button.addEventListener('click', function () {
+    document.querySelectorAll('.trailers__button').forEach(button => {
+      button.classList.remove('trailers__button_active');
+    });
+    this.classList.add('trailers__button_active');
+  });
+});
 
+// Получаем все кнопки трейлеров и все видео
+const buttons = document.querySelectorAll('.trailers__button');
+const videos = document.querySelectorAll('.trailers__video');
+
+// Добавляем обработчик события click для каждой кнопки
+buttons.forEach((button, index) => {
+  button.addEventListener('click', function () {
+    // Скрываем все видео
+    videos.forEach(video => {
+      video.style.display = 'none';
+    });
+
+    // Находим индекс кнопки, по которой было совершено нажатие
+    const buttonIndex = Array.from(buttons).indexOf(button);
+
+    // Показываем соответствующее видео
+    videos[buttonIndex].style.display = 'block';
+  });
+});
